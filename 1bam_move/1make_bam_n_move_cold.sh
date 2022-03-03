da393425f61b79b5ed8b4b2000727e1c9cee1d86
Yiwen Chen <brandonchen2932@outlook.com>
2022-03-03 11:23:33 +0100

    fixed a bug

--- 1bam_move/1make_bam_n_move_cold.sh
+++ 1bam_move/1make_bam_n_move_cold.sh
@@ -4,7 +4,7 @@ do
   for cramfile in $(find /Volumes/Data/${pool}/a/ -type f -maxdepth 1 -name "*.cram" -exec basename {} \;)
   do
     cramfilename=${cramfile%.*}
-    bamfilename=$(awk -v var="$cramfilename" '$1==var{print $2}' ~/COLD/pipeline/1bam_move/cram2bam_name.txt)
+    bamfilename=$(awk -v var="$cramfilename" '$1==var{print $2}' ~/COLD/pipeline_cold/1bam_move/cram2bam_name.txt)
     if [ ! -d ~/COLD/data/${bamfilename}/ ]; then
       mkdir ~/COLD/data/${bamfilename}/
     fi
@@ -19,7 +19,7 @@ do
       for cramfile in $(find /Volumes/Data/${pool}/${x}/ -type f -maxdepth 1 -name "*.cram" -exec basename {} \;)
       do
         cramfilename=${cramfile%.*}
-        bamfilename=$(awk -v var="$cramfilename" '$1==var{print $2}' ~/COLD/pipeline/1bam_move/cram2bam_name.txt)
+        bamfilename=$(awk -v var="$cramfilename" '$1==var{print $2}' ~/COLD/pipeline_cold/1bam_move/cram2bam_name.txt)
         samtools view -b -q 20 -F 0x400 -T ~/COLD/reference/dsimM252v1.2+microbiome.fa -o ~/COLD/data/${bamfilename}/${bamfilename}_${x}.bam /Volumes/Data/${pool}/${x}/${cramfile}
       done
     fi
