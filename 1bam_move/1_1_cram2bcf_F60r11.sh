829f138e5f8496cbe8a4d4dee6010f8babc4d0f3
Yiwen Chen <brandonchen2932@outlook.com>
2022-03-07 13:50:08 +0100

    add absolute path

--- 1bam_move/1_1_cram2bcf_F60r11.sh
+++ 1bam_move/1_1_cram2bcf_F60r11.sh
@@ -11,13 +11,13 @@ ref_fa="/Users/ychen/COLD/reference/dsimM252v1.2+microbiome.fa"
 max_cov=400
 # input path (if your samples have been sequenced on multiple lanes, the CRAMs
 # for a single sample need to be samtools-merged into one file instead, see below)
-in_folder_329="/Users/ychen/COLD/data/coldF60r11_1"
-in_folder_356="/Users/ychen/COLD/data/coldF60r11_2"
+in_folder_329="/Volumes/Data/329"
+in_folder_356="/Volumes/Data/356"
 
 # 1.a convert to SAM (needed by awk scripts)
 # samtools view -h "$in_cram" |\
 # or with multiple CRAMs for the same sample
-samtools merge -O SAM -o - "$in_folder_329/coldF60r11_1_a.bam" "$in_folder_329/coldF60r11_1_b.bam" "$in_folder_356/coldF60r11_2_a.bam" |\
+samtools merge -O SAM -o - "$in_folder_329/a/LB_329.D707.cram" "$in_folder_329/b/LB_329.D707.cram" "$in_folder_356/a/LB_356.D707+D507.cram" |\
   # 1.b flag short fragments  (replace $rl with the actual read length)
   /Users/ychen/COLD/pipeline_cold/1bam_move/rupert_pipe_v2/flag-short.awk -v READ_LENGTH=$read_length |\
   # 1.c recalculate mapping quality
