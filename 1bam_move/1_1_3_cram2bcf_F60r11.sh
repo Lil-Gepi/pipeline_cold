381c78e5325d4027ead47e19cecfa71981f35bc3
Yiwen Chen <brandonchen2932@outlook.com>
2022-03-10 09:58:28 +0100

    get no rl working

--- 1bam_move/1_1_3_cram2bcf_F60r11.sh
+++ 1bam_move/1_1_3_cram2bcf_F60r11.sh
@@ -1,27 +1,35 @@
 ## 1. Prepare the raw single-sample pileup
 
 # adjust these variables as needed
+
+cold_dir="/Users/ychen/COLD"
 # read length
-# read_length=150
+#rl_329=150
+#rl_356=125
 # output path (I use BCF, the compressed binary version of VCF, here)
-out_bcf="~/COLD/data/coldF60_r11/coldF60_r11_noreadlength_raw.bcf"
+out_bcf="${cold_dir}/data/coldF60_r11/coldF60_r11_norl_raw.bcf"
 # reference genome in FASTA format
-ref_fa="~/COLD/reference/dsimM252v1.2+microbiome.fa"
+ref_fa="${cold_dir}/reference/dsimM252v1.2+microbiome.fa"
 # max. depth to consider, set to 5x expected autosomal coverage
 max_cov=400
 # input path (if your samples have been sequenced on multiple lanes, the CRAMs
 # for a single sample need to be samtools-merged into one file instead, see below)
-in_folder_329="~/COLD/data/coldF60r11_1"
-in_folder_356="~/COLD/data/coldF60r11_2"
+in_dir_329="/Volumes/Data/329"
+in_dir_356="/Volumes/Data/356"
 
 # 1.a convert to SAM (needed by awk scripts)
 # samtools view -h "$in_cram" |\
 # or with multiple CRAMs for the same sample
-samtools merge -O SAM -o - "$in_folder_329/coldF60r11_1_a.cram" "$in_folder_329/coldF60r11_1_b.cram" "$in_folder_356/coldF60r11_2_a.bam" |\
+samtools merge -O SAM -o - \
+<(samtools view -T "$ref_fa" -h -F 0x400 "$in_dir_329/a/LB_329.D707.cram" | ./rupert_pipe_v2/flag-short.awk ) \
+<(samtools view -T "$ref_fa" -h -F 0x400 "$in_dir_329/b/LB_329.D707.cram" | ./rupert_pipe_v2/flag-short.awk ) \
+<(samtools view -T "$ref_fa" -h -F 0x400 "$in_dir_356/a/LB_356.D707+D507.cram" | ./rupert_pipe_v2/flag-short.awk )|\
+   samtools collate -Ouf - | samtools fixmate -ru - - | samtools view -f 0x2 -h - |\
   # 1.b flag short fragments  (replace $rl with the actual read length)
-  ./flag-short.awk -v  |\
+  # ./rupert_pipe_v2/flag-short.awk -v READ_LENGTH=$read_length |\
   # 1.c recalculate mapping quality
-  ./remapq.awk -v NORD=7.5 |\
+  ./rupert_pipe_v2/remapq.awk -v NORD=7.5 |\
+  samtools sort -@3 -m 4G -u -T /tmp/sortsam.$(basename ${out_bcf%.bcf}).$tag - |\
   # 1.d make single-sample pileup
   # --incl-flags 0x2 ... include only proper pairs
   # -q 10 ... include only reads with a mapping quality that apsses the threshold
@@ -30,11 +38,15 @@ samtools merge -O SAM -o - "$in_folder_329/coldF60r11_1_a.cram" "$in_folder_329/
   # -Q 20 ... count only bases with min(base quality,BAQ) passing the threshold
   # -D    ... enable BAQ calculation for all positions (check bcftools manpage for details)
   # -a DP,AD ... annotate with additional fields (AD = allele depth for each allele is needed below!)
-  bcftools mpileup -f "$ref_fa" -d $max_cov --incl-flags 0x2 -q 10 -Q 20 -D -a DP,AD,QS,SCR -Ob - > "$out_bcf"
+  bcftools mpileup -f "$ref_fa" -d $max_cov --skip-all-unset 0x2 -q 10 -Q 20 -D -a DP,AD,QS,SCR -Ob --threads 3 - > "$out_bcf"
 
 ## 2. Annotate raw single-sample pileup with more FORMAT tags (to save the information from INFO)
 # input path (a pileup generated in step 1 in BCF, VCF, or bgzipped VCF format, cannot be a pipe!)
-in_bcf="~/COLD/data/coldF60_r11/coldF60_r11_noreadlength_raw.bcf"
+in_bcf="${cold_dir}/data/coldF60_r11/coldF60_r11_norl_raw.bcf"
 # output path (use - to write uncompressed BCF to stdout, or leave empty to write VCF to stdout)
-out_bcf="~/COLD/data/coldF60_r11/coldF60_r11_noreadlength_pre.bcf"
-./pre-merging.sh "$in_bcf" "$out_bcf"
+out_bcf="${cold_dir}/data/coldF60_r11/coldF60_r11_norl_pre.bcf"
+#./rupert_pipe_v2/pre-merging.sh "$in_bcf" "$out_bcf"
+bcftools annotate -Ov -x INFO/IMF,INFO/VDB,INFO/RPBZ,INFO/MQBZ,INFO/MQSBZ,INFO/SCBZ,INFO/BQBZ,INFO/FS,INFO/SGB,INFO/I16,INFO/QS,INFO/MQ0F,FORMAT/PL "$in_bcf" |\
+ ./rupert_pipe_v2/info2fmt.awk -v tags=DP > "$out_bcf"
+
+exit
